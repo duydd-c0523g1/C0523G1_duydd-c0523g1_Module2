76e37b0d6ea7a77af713bbc6b8251da4b84bd841
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-07-06 14:59:50 +0700

    1st commit

--- duy/src/ss2_loop/bai_tap/Shapes.java
+++ duy/src/ss2_loop/bai_tap/Shapes.java
@@ -1,16 +1,14 @@
 package exercise;
 
 import java.util.Scanner;
+
 public class Shapes {
     public static void main(String[] args) {
-
         System.out.println("Nhập 1 để vẽ hình chữ nhật");
         System.out.println("Nhập 2 để vẽ hình tam giác vuông bottom-left");
         System.out.println("Nhập 3 để vẽ hình tam giác vuông top-left");
         Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
-
-
         if (input == 1) {
             for (int i = 0; i < 3; i++) {
                 System.out.println();
