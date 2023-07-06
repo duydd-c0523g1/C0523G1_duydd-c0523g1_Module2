389dd93e99b942bcba5512371c64286a138aacf8
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-07-06 15:55:43 +0700

    1st commit

--- duy/src/ss3_array_method/bai_tap/CharFrequency.java
+++ duy/src/ss3_array_method/bai_tap/CharFrequency.java
@@ -1,16 +1,15 @@
 package ss3_array_method.bai_tap;
 
 import java.util.Scanner;
+
 public class CharFrequency {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String str = "awdawdoijfdsf oijsad aosdij awdiojad asoawjdosiajdaw ajsoasd";
-
+        String str = "awd1awdoijfdsf oijsad aosdij awdioj87ad asoaw321654jdosiajdaw ajsoasd";
         //Nhập vào ký tự cần kiểm tra
         do {
             System.out.println("Nhập vào ký tự cần kiểm tra");
             char charInput = scanner.nextLine().charAt(0);
-
             //Duyệt qua String ở trên
             int count = 0;
             for (int i = 0; i < str.length(); i++) {
