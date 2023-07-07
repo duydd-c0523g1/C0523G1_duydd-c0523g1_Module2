b64a5ac5b3f145b624307f254d7eeea886e7f980
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-07-07 14:54:46 +0700

    1st commit

--- duy/src/ss4_class_object/bai_tap/bai_2_stop_watch/Main.java
+++ duy/src/ss4_class_object/bai_tap/bai_2_stop_watch/Main.java
@@ -1,4 +1,36 @@
 package ss4_class_object.bai_tap.bai_2_stop_watch;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        //Khởi tạo mảng random 0 -> 100000
+        int[] arr = new int[100000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * arr.length);
+        }
+        //Tạo đối tượng StopWatch
+        StopWatch stopWatch = new StopWatch();
+        //Tạo function selection sort
+        selectionSort(arr); //Gọi hàm selectionSort
+        //Dừng đồng hồ và cho ra kết quả
+        stopWatch.stop();
+        System.out.println("Thời gian thực hiện thuật toán: " + stopWatch.getElapsedTime() + "ms");
+    }
+
+    //Tạo function selectionSort
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = arr[j];
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
--- duy/src/ss4_class_object/bai_tap/bai_2_stop_watch/StopWatch.java
+++ duy/src/ss4_class_object/bai_tap/bai_2_stop_watch/StopWatch.java
@@ -1,5 +1,30 @@
 package ss4_class_object.bai_tap.bai_2_stop_watch;
 
 public class StopWatch {
+    private long startTime;
+    private long endTime;
 
+    public StopWatch() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
 }
