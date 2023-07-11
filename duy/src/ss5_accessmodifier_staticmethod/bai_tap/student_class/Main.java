1a6bc00e257fd8a5bf9a0b7ce80c3be62a1f2935
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-07-11 20:47:13 +0700

    1st commit

---
+++ duy/src/ss5_accessmodifier_staticmethod/bai_tap/student_class/Main.java
@@ -0,0 +1,11 @@
+package ss5_accessmodifier_staticmethod.bai_tap.student_class;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        System.out.println(student1);
+        student1.setName("Lien");
+        student1.setClasses("C0523G1");
+        System.out.println(student1);
+    }
+}
