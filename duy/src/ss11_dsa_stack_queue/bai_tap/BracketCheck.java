27cbdc51f9e6d3e65f3f2600499b693ba688b4d2
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-07-17 16:52:00 +0700

    1st commit

---
+++ duy/src/ss11_dsa_stack_queue/bai_tap/BracketCheck.java
@@ -0,0 +1,35 @@
+package ss11_dsa_stack_queue.bai_tap;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BracketCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào biểu thức cần kiểm tra:");
+        String equation = scanner.nextLine();
+        char[] charArray = equation.toCharArray();
+        for (char c : charArray) {
+            System.out.print(c + " ");
+        }
+        Stack<Character> bStack = new Stack<>();
+        for (char c : charArray) {
+            if (c == '(') {
+                bStack.push(c);
+            } else if (c == ')') {
+                if (bStack.empty()) {
+                    System.out.println("Not good");
+                    break;
+                }else {
+                    bStack.pop();
+                }
+            }
+        }
+        if (bStack.isEmpty()) {
+            System.out.println("Good");
+        }else {
+            System.out.println("Not good");
+        }
+    }
+}
