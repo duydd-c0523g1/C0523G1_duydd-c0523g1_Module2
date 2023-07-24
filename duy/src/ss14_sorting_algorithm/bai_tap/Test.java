b7fdd706af4450f949209a7a67fe904867163730
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-07-24 15:18:35 +0700

    1st commit

--- duy/src/ss14_sorting_algorithm/bai_tap/Test.java
+++ duy/src/ss14_sorting_algorithm/bai_tap/Test.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         int arr[] = {11, 5, 3, 4, 6};
-        SelectionSort.sort(arr);
+        InsertionSort.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
