d57d0257bb38c61a865faad96601c98a5d621bca
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-07-24 15:28:48 +0700

    1st commit

---
+++ duy/src/ss15_exception_handling/bai_tap/IllegalTriangleException.java
@@ -0,0 +1,8 @@
+package ss15_exception_handling.bai_tap;
+
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+}
+
