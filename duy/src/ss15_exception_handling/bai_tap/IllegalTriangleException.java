0d1dd7eba64423e37c57617b8757566992a78ba2
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-07-25 11:08:53 +0700

    1st commit

--- duy/src/ss15_exception_handling/bai_tap/IllegalTriangleException.java
+++ duy/src/ss15_exception_handling/bai_tap/IllegalTriangleException.java
@@ -5,4 +5,3 @@ public class IllegalTriangleException extends Exception {
         super(message);
     }
 }
-
---
+++ duy/src/ss15_exception_handling/bai_tap/Test.java
@@ -0,0 +1,27 @@
+package ss15_exception_handling.bai_tap;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Nhập độ dài cạnh thứ nhất:");
+            double side1 = scanner.nextDouble();
+            System.out.println("Nhập độ dài cạnh thứ hai:");
+            double side2 = scanner.nextDouble();
+            System.out.println("Nhập độ dài cạnh thứ ba:");
+            double side3 = scanner.nextDouble();
+
+            Triangle triangle = new Triangle(side1, side2, side3);
+            System.out.println("Tam giác hợp lệ.");
+        } catch (IllegalTriangleException e) {
+            System.out.println("Ngoại lệ: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ngoại lệ không xác định: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
