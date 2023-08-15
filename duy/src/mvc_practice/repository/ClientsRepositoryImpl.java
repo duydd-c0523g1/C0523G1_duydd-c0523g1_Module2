ddaa5f339e8e136f9eaab72d7a96dbf8e2d2b1b4
duydd-c0523g1 <daoducduy.work@gmail.com>
2023-08-15 14:18:45 +0700

    1st commit

--- duy/src/mvc_practice/repository/ClientsRepositoryImpl.java
+++ duy/src/mvc_practice/repository/ClientsRepositoryImpl.java
@@ -17,7 +17,7 @@ public class ClientsRepositoryImpl implements IClientsRepository {
         clientsList.add(clients);
     }
 
-    @Overrid2e
+    @Override
     public void removeCustomerById(int id) {
         clientsList.removeIf(customer -> customer.getId() == id);
     }
